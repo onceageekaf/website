716c65fdebd4c1232e32c913b43e6f419eb580a5
onceageekaf <ash@atomfactory.io>
2026-05-29 10:38:36 +0200

    Remove DeepTechInfrastructureCtaSection
    Removed DeepTechInfrastructureCtaSection from the main layout.

--- app/page.tsx
+++ app/page.tsx
@@ -197,7 +197,7 @@ export default function page() {
 
       <ContractLogicEngineSection />
       <CleEngagementSection />
-      <DeepTechInfrastructureCtaSection />
+      
       <Footer />
     </main>
   )
